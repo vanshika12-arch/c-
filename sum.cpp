63af108ae5034517b905c9895aba7090091b6506
vanshika singh <itzkhushisingh12@gmail.com>
2026-07-30 00:07:28 +0530

    sum of two numbers

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum;
+    cout<<"enter first number";
+    cin>>num1;
+    cout<<"enter second number";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<"sum of two numbers is"<<sum;
+    return 0;
+}
