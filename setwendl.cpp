a8acb771e5ef4f0af60cbc5e0a6a9a423bb12edb
vanshika singh <itzkhushisingh12@gmail.com>
2026-08-16 21:39:30 +0530

    useofsetw and endl manipulator

---
+++ setwendl.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+#include<iomanip>
+int main()
+{
+    cout<<"Name"<<setw(15)<<"roll no "<<setw(15)<<"marks"<<endl;
+    cout<<"vanshika"<<setw(11)<<32<<setw(16)<<95<<endl;
+    cout<<"rishabh"<<setw(14)<<21<<setw(16)<<85<<endl;
+    cout<<"naira"<<setw(14)<<11<<setw(16)<<90<<endl;
+    return 0;
+}
